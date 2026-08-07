d3203b7468fd8a4107bcc9cee8cd83b9091dddb3
Miguel <santisanty2006@gmail.com>
2026-08-07 12:40:33 -0500

    Organizacion de router de usuarios y destinos

--- Backend/routers/destinos.py
+++ Backend/routers/destinos.py
@@ -2,29 +2,32 @@ from fastapi import APIRouter
 from models.destino import Destino
 import services.destino_service as destino_service
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/destinos",
+    tags=["Destinos"]
+)
 
-@router.get("/destinos")
+@router.get("/")
 def listar_destinos():
 
     return destino_service.listar_destinos()
 
-@router.get("/destinos/{id}")
+@router.get("/{id}")
 def buscar_destino_por_id(id: int):
 
     return destino_service.buscar_destino_por_id(id)
 
-@router.post("/destinos")
+@router.post("/")
 def crear_destino(destino: Destino):
 
     return destino_service.crear_destino(destino)
 
-@router.put("/destinos/{id}")
+@router.put("/{id}")
 def actualizar_destino(id: int, destino: Destino):
 
     return destino_service.actualizar_destino(id, destino)
 
-@router.delete("/destinos/{id}")
+@router.delete("/{id}")
 def eliminar_destino(id: int):
 
     return destino_service.eliminar_destino(id)
--- Backend/routers/usuarios.py
+++ Backend/routers/usuarios.py
@@ -3,29 +3,32 @@ from models.usuario import Usuario
 from database.conexion import obtener_conexion
 import services.usuario_service as usuario_service
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/usuarios",
+    tags=["Usuarios"]
+)
 
-@router.post("/usuarios")
+@router.post("/")
 def crear_usuario(usuario: Usuario):
 
     return usuario_service.crear_usuario(usuario)
 
-@router.get("/usuarios")
+@router.get("/")
 def listar_usuarios():
 
     return usuario_service.obtener_usuarios()
 
-@router.get("/usuarios/{id}")
+@router.get("/{id}")
 def obtener_usuario_por_id(id: int):
 
     return usuario_service.obtener_usuario_por_id(id)
 
-@router.put("/usuarios/{id}")
+@router.put("/{id}")
 def actualizar_usuario(id: int, usuario: Usuario):
 
     return usuario_service.actualizar_usuario(id, usuario)
 
-@router.delete("/usuarios/{id}")
+@router.delete("/{id}")
 def eliminar_usuario(id: int):
 
     return usuario_service.eliminar_usuario(id)
